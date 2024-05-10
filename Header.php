d3f26a8795ab4dd161d73331f52f85a3c194eef0
bhakti <bhaktiwadkar.riyality@gmail.com>
2024-05-10 16:25:39 +0530

    templateweb

---
+++ Header.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+  
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <!-- Bootstrap CSS-->
+    <link href="assets/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file://-->
+    <!-- IE 9-->
+    <!-- Vendors-->
+    <link rel="stylesheet" href="assets/vendors/flexslider/flexslider.min.css">
+    <link rel="stylesheet" href="assets/vendors/swipebox/css/swipebox.min.css">
+    <link rel="stylesheet" href="assets/vendors/slick/slick.min.css">
+    <link rel="stylesheet" href="assets/vendors/slick/slick-theme.min.css">
+    <link rel="stylesheet" href="assets/vendors/animate.min.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-datepicker/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="assets/vendors/pageloading/css/component.min.css">
+    <link rel="stylesheet" href="assets/vendors/dialog/css/dialog.css">
+    <!-- Font-icon-->
+    <link rel="stylesheet" href="assets/fonts/font-icon/style.css">
+    <!-- Style-->
+    <link rel="stylesheet" type="text/css" href="assets/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/elements.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/extra.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/widget.css">
+    <link id="colorpattern" rel="stylesheet" type="text/css" href="assets/css/color/colordefault.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/live-settings.css">
+    <!-- Google Font-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+    <!-- Script Loading Page-->
+    <script src="assets/vendors/html5shiv.js"></script>
+    <script src="assets/vendors/respond.min.js"></script>
+    <script src="assets/vendors/pageloading/js/snap.svg-min.js"></script>
+    <script src="assets/vendors/pageloading/sidebartransition/js/modernizr.custom.js"></script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-BYR30Q9JF8"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-BYR30Q9JF8');
+	</script>
+  </head>
+  <body>
+    <div id="pagewrap" class="pagewrap">
+      <div id="html-content" class="wrapper-content">
+        <header>
+          <div class="header-top top-layout-02">
+            <div class="container">
+              <div class="topbar-left">
+                <div class="topbar-content">
+                  <div class="item"> 
+                    <div class="wg-contact"><i class="fa fa-map-marker"></i><span>Hadapsar, Pune City 411028</span></div>
+                  </div>
+                  <div class="item"> 
+                    <div class="wg-contact"><i class="fa fa-phone"></i><span>012 345 678</span></div>
+                  </div>
+                </div>
+              </div>
+              <div class="topbar-right">
+                <div class="topbar-content">
+                  <div class="item">
+                    <ul class="socials-nb list-inline wg-social">
+                      <li><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+                      <li><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+                      <li><a href="javascript:void(0)"><i class="fa fa-pinterest"></i></a></li>
+                      <li><a href="javascript:void(0)"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                  </div>
+                  <div class="item">
+                    <div class="wg-social"><i class="fa fa-user"></i><a href="javascript:void(0)">My Account</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="header-main">
+            <div class="container">
+              <div class="open-offcanvas">&#9776;</div>
+              <div class="utility-nav">
+                <div class="dropdown dropdown-full-mobile"><a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="search-bar dropdown-toggle"><i class="fa fa-shopping-bag"></i></a>
+                  <div class="dropdown-menu">
+                    <div class="cart_lite">
+                      <div class="cart_lite_list">
+                        <ul>
+                          <li class="cart_lite_item"><a href="product-single.html" class="cart_item_thumbnail"><img width="150" height="100" src="assets/images/product/product-circle-1h.jpg" alt="" class="img-responsive"></a>
+                            <div class="cart_item_summary"><a href="product-single.html" class="cart_item_title">Caramel Frappuccino</a><span class="product-price-amount"><span class="quantity">1 × </span><span class="product-price-currencySymbol">Rs</span>120</span></div><a href="#" class="remove_from_cart_button">×</a>
+                          </li>
+                          <li class="cart_lite_item"><a href="product-single.html" class="cart_item_thumbnail"><img width="150" height="100" src="assets/images/product/product-circle-1k.jpg" alt="" class="img-responsive"></a>
+                            <div class="cart_item_summary"><a href="product-single.html" class="cart_item_title">Chocolate Fudge Ripple</a><span class="product-price-amount"><span class="quantity">2 × </span><span class="product-price-currencySymbol">Rs</span>240</span></div><a href="#" class="remove_from_cart_button">×</a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="cart_lite_total">
+                        <p><strong>Subtotal: </strong><span class="product-price-amount"><span class="product-price-currencySymbo">Rs</span>600</span></p>
+                      </div>
+                      <div class="cart_lite_button"><a href="product-cart.php" class="swin-btn btn-sm"><span>View Cart</span></a><a href="product-checkout.php" class="swin-btn btn-sm"><span>Checkout</span></a></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="dropdown"><a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="search-bar dropdown-toggle"><i class="fa fa-search"></i></a>
+                  <div class="dropdown-menu">
+                    <div class="search-form">
+                      <form action="#">
+                        <div class="input-group">
+                          <input type="text" placeholder="Search" class="form-control">
+                          <div class="input-group-addon"><i class="fa fa-search"></i></div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="header-logo"><a href="index.php" class="logo"><img src="assets/images/logo.png" alt="fooday" class="logo-img"></a></div>
+              <nav id="main-nav-offcanvas" class="main-nav-wrapper">
+                <div class="close-offcanvas-wrapper"><span class="close-offcanvas">x</span></div>
+                <div class="main-nav">
+                  <ul id="main-nav" class="nav nav-pills">
+                    <li class="dropdown current-menu-item"><a href="index.php" class="dropdown-toggle">Home </a>
+                      <i class="fa fa-angle-down btn-open-dropdown"></i>
+                      
+                    </li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="reservation.php">Reservation</a></li>
+                    <li class="dropdown"><a href="menu-classic.php" class="dropdown-toggle">Menu</a><i class="fa fa-angle-down btn-open-dropdown"></i>
+<!--                       <ul class="dropdown-menu">
+                        <li><a href="menu-classic.html">Menu Classic</a></li>
+                        <li><a href="menu-grid-1.html">Menu Grid 01</a></li>
+                        <li><a href="menu-grid-2.html">Menu Grid 02</a></li>
+                        <li><a href="menu-grid-3.html">Menu Grid 03</a></li>
+                        <li><a href="product-single.html">Product Detail</a></li>
+                        <li><a href="product-cart.html">Product Cart</a></li>
+                        <li><a href="product-checkout.html">Product Checkout</a></li>
+                      </ul> -->
+                    </li>
+                    <li><a href="blogs.php">Blog</a></li>
+
+                <li><a href="contact.php">Contact</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </header>
